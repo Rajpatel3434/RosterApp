f6ac30c6602e2a9f02fdbf9d11ca174b621cffb1
Rajpatel3434 <rajp4281@gmail.com>
2024-01-07 15:40:36 +1030

    Toolbar on start

--- src/main/java/com/example/rosterapp/HelloApplication.java
+++ src/main/java/com/example/rosterapp/HelloApplication.java
@@ -3,18 +3,26 @@ package com.example.rosterapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setMaximized(true);
+        stage.setTitle("Roster Manager App");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/rosterapp/HelloController.java
+++ src/main/java/com/example/rosterapp/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.rosterapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label statusLabel;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
     }
+
+    // Method to update the status label
+
+
 }
